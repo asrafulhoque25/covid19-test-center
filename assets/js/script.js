6a9425bea5a1969d4511165d9d4f4e4f236620c3
Asraful Hoque <96014879+asrafulhoque25@users.noreply.github.com>
2022-10-20 10:53:39 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/**
+ * @Script js for (Template/Covid-19 test center search)
+ *
+ * @project     - Covid-19 test center search
+ * @author      - Andreas
+ * @created_by  - Md Afzal Hossain Patwary
+ * @created_at  - Andreas
+ * @modified_by - 
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // wow js init
+    $(function () {
+        var wow = new WOW({
+            animateClass: 'animated',
+            mobile: true
+        });
+        wow.init();
+    });
+
+});
